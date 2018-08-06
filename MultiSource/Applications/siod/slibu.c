340603fe4ac2f7ca1b0253753d620ee25da273a3
Kazushi (Jam) Marukawa <kaz-marukawa@xr.jp.nec.com>
2018-08-06 16:06:13 +0900

    Modify test-suite code to support VE architecture.

--- MultiSource/Applications/siod/slibu.c
+++ MultiSource/Applications/siod/slibu.c
@@ -723,7 +723,7 @@ LISP l_readdir(LISP v)
  if (!r) return(NIL);
 #if defined(sun) || defined(sgi) || defined(linux)
  namlen = safe_strlen(r->d_name,r->d_reclen);
-#elif defined(__CYGWIN__)
+#elif defined(__CYGWIN__) || defined(__ve__)
  namlen = strlen(r->d_name);
 #else
  namlen = r->d_namlen;
--- MultiSource/Benchmarks/MiBench/office-ispell/term.c
+++ MultiSource/Benchmarks/MiBench/office-ispell/term.c
@@ -72,7 +72,7 @@ static char Rcs_Id[] =
 #include "msgs.h"
 #include <signal.h>
 
-#if defined(__GLIBC__) || defined(__FreeBSD__) || defined(__APPLE__)
+#if defined(__GLIBC__) || defined(__FreeBSD__) || defined(__APPLE__) || defined(__ve__)
 /* Use termios under at least glibc */
 #include <termios.h>
 #define USE_TERMIOS
